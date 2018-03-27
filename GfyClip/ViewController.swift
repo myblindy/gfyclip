0fff8d8884544f2e2ab3dd4b315daa6e9b696af5
Marius Mitea <tweety.04.01@gmail.com>
2018-03-26 22:22:26 -0400

    -

--- GfyClip/ViewController.swift
+++ GfyClip/ViewController.swift
@@ -9,17 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var urlField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func urlFieldEditingDidChange(_ sender: Any) {
+        startButton.isEnabled = urlField.text != nil && !urlField.text!.isEmpty
     }
-
-
 }
-
