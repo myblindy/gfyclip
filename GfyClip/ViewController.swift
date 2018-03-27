6a1c5b632fe663362e5d1bcd98848607662741fb
Marius Mitea <tweety.04.01@gmail.com>
2018-03-26 21:27:19 -0400

    Initial Commit

---
+++ GfyClip/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GfyClip
+//
+//  Created by Marius Mitea on 2018-03-26.
+//  Copyright © 2018 Marius Mitea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
